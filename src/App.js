2648165406c4674d6b02567d72b18625931902fd
Esneider Ospina <sneuder8@gmail.com>
2022-04-23 17:09:45 -0500

    Adding components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
 
 import styled from "styled-components";
+
+import Addbar from "./components/addbar/Addbar";
 import Todos from "./components/Todos";
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
         <Sections to='/active'>Active</Sections>
         <Sections to='/completed'>Completed</Sections>
       </Nav>
+      <Addbar />
       <Routes>
         <Route path="/" element={<Todos />} />
         <Route path="/active" element={<Todos />} />
