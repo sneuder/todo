36d5559c202cab3dcdcffae6ec522adfe4b0fb99
Esneider Ospina <sneuder8@gmail.com>
2022-04-23 14:37:02 -0500

    Basics of Todos

---
+++ src/components/Todos.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import { getTodos } from "../utils/utils";
+
+import styled from "styled-components";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const pathname = useLocation().pathname;
+
+  useEffect(() => {
+    if (pathname === "/") {
+    }
+
+    if (pathname === "/active") {
+    }
+
+    if (pathname === "/completed") {
+    }
+  }, [pathname]);
+
+  return (
+    <div>
+      <ContainerTodo>
+        <Input type="checkbox" placeholder="Add Todo" />
+        <Label>Hola</Label>
+      </ContainerTodo>
+    </div>
+  );
+};
+
+const ContainerTodo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #828282;
+  border-radius: 0.2rem;
+  cursor: pointer;
+
+  &:checked {
+    background-color: #828282;
+  }
+`;
+
+const Label = styled.label`
+  margin-left: 0.5rem;
+`;
+
+export default Todos;
