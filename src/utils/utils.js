50e11ec7a2348c3ebf6351cec84c8c2ee3b1cc03
Esneider Ospina <sneuder8@gmail.com>
2022-04-23 17:03:34 -0500

    Toggle false and true

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,40 @@
 export const getTodos = (filter) => {
-  let todos = JSON.parse(localStorage.getItem('todos-todoapp')) || [];
+  let todos = JSON.parse(localStorage.getItem("todos-todoapp")) || [];
 
   switch (filter) {
-    case 'all': return todos;
-    case 'active': return todos.filter(todo => todo.done !== true);
-    case 'completed': return todos.filter(todo => todo.done !== false);
+    case "all":
+      return todos;
+    case "active":
+      return todos.filter((todo) => todo.done !== true);
+    case "completed":
+      return todos.filter((todo) => todo.done !== false);
   }
-}
+};
 
 export const addTodos = (todo) => {
-  let todos = JSON.parse(localStorage.getItem('todos-todoapp')) || [];
+  let todos = JSON.parse(localStorage.getItem("todos-todoapp")) || [];
   todos.unshift(todo);
-  localStorage.setItem('todos-todoapp', JSON.stringify(todos));
-}
+  localStorage.setItem("todos-todoapp", JSON.stringify(todos));
+};
+
+export const completeTodo = (todo) => {
+  let todos = JSON.parse(localStorage.getItem("todos-todoapp")) || [];
+  todos.map((t) => {
+    if (t.id === todo.id) {
+      
+      if (t.done === true) {
+        t.done = false;
+        return t;
+      }
+
+      if (t.done === false) {
+        t.done = true;
+        return t;
+      }
+    }
+
+    return t;
+  });
+
+  localStorage.setItem("todos-todoapp", JSON.stringify(todos));
+};
