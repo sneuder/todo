bf3dc0fd0a412213e884a166bf2bcf418033badb
Esneider Ospina <sneuder8@gmail.com>
2022-04-23 11:52:44 -0500

    Importing components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
+
 function App() {
   return (
     <div>
+      <Header>#todo</Header>
+      <Nav >
+        <p>Hola</p>
+        <p>Hola</p>
+        <p>Hola</p>
+      </Nav>
     </div>
   );
 }
