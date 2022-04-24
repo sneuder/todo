ef37054ada4a359c7db274f7feb91f5246d3cddd
Esneider Ospina <sneuder8@gmail.com>
2022-04-24 12:10:09 -0500

    Global store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-import './index.css';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
