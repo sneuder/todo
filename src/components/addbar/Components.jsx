0260588c764ddf6c6db76f2c8e9c0bdb0b9b1ada
Esneider Ospina <sneuder8@gmail.com>
2022-04-24 12:14:24 -0500

    individual components for addbar component

---
+++ src/components/addbar/Components.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+export const Form = styled.form`
+  display: flex;
+  margin: 0.5rem 0;
+`
+
+export const InputText = styled.input`
+  width: 100%;
+  height: 3rem;
+  padding-left: 1rem;
+  margin-right: 1rem;
+  border-radius: 0.3rem;
+  border: 1px solid #828282;
+`
+
+export const InputSubmit = styled.input`
+  padding: 0 1rem;
+  cursor: pointer;
+  border-radius: 0.3rem;
+  border: none;
+  background-color: #2F80ED;
+  color: #fff;
+  font-weight: 600;
+`
